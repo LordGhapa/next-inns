f8fcca7f74b4becfb0b99d356d0bb6cc94d7f7d5
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-12-31 21:50:31 -0300

    💄 UI: component book room cta for page room details

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -10,21 +10,83 @@ import { MdOutlineCleaningServices } from "react-icons/md";
 import { AiOutlineMedicineBox } from "react-icons/ai";
 import { LiaFireExtinguisherSolid } from "react-icons/lia";
 import { GiSmokeBomb } from "react-icons/gi";
+import BookRoomCta from "@/components/BookRoomCta";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 export default function RoomDetails(props: { params: { slug: string } }) {
   const {
     params: { slug },
   } = props;
 
+  const [checkinDate, setCheckinDate] = useState<Date | null>(null);
+  const [checkoutDate, setCheckoutDate] = useState<Date | null>(null);
+  const [adults, setAdults] = useState<number>(1);
+  const [noOfChildren, setNoOfChildren] = useState<number>(0);
+
   const fetchRoom = async () => await getRoom(slug);
-
   const { data: room, error, isLoading } = useSWR("/api/room", fetchRoom);
-
   if (error) throw new Error("Erro ao fazer fetch da pagina rooms details");
   if (typeof room === "undefined" && !isLoading)
     throw new Error("Erro ao fazer fetch da pagina rooms details");
-
   if (!room) return <Loading />;
+
+  const calcMinCheckoutDate = () => {
+    if (checkinDate) {
+      const nextDay = new Date(checkinDate);
+      nextDay.setDate(nextDay.getDate() + 1);
+
+      return nextDay;
+    }
+    return null;
+  };
+
+  const calcNumDays = () => {
+    if (!checkinDate || !checkoutDate) return;
+    const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
+
+    const noOfDays = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    return noOfDays;
+  };
+
+  const handleBookNowClick = async () => {
+    if (!checkinDate || !checkoutDate)
+      return toast.error("Coloque data de Entrada e Saida");
+
+    if (checkinDate > checkoutDate)
+      return toast.error("Coloque um valor Valido");
+
+    const numberOfDays = calcNumDays();
+
+    const hotelRoomSlug = room.slug.current;
+
+    const stripe = await getStripe();
+
+    try {
+      const { data: stripeSession } = await axios.post("/api/stripe", {
+        checkinDate,
+        checkoutDate,
+        adults,
+        children: noOfChildren,
+        numberOfDays,
+        hotelRoomSlug,
+      });
+
+      if (stripe) {
+        const result = await stripe.redirectToCheckout({
+          sessionId: stripeSession.id,
+        });
+
+        if (result.error) {
+          toast.error("Payment Failed");
+        }
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+      toast.error("Ocorreu um Error");
+    }
+  };
 
   return (
     <div>
@@ -113,6 +175,23 @@ export default function RoomDetails(props: { params: { slug: string } }) {
           </div>
           <div className="sticky top-10 h-fit overflow-auto rounded-xl shadow-lg dark:shadow dark:shadow-white md:col-span-4 ">
             {/* book room cta */}
+            <BookRoomCta
+              checkinDate={checkinDate}
+              setCheckinDate={setCheckinDate}
+              checkoutDate={checkoutDate}
+              setCheckoutDate={setCheckoutDate}
+              discount={room.discount}
+              price={room.price}
+              specialNote={room.specialNote}
+              calcMinCheckoutDate={calcMinCheckoutDate}
+              adults={adults}
+              setAdults={setAdults}
+              noOfChildren={noOfChildren}
+              setNoOfChildren={setNoOfChildren}
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              handleBookNowClick={handleBookNowClick}
+              isBooked={room.isBooked}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/BookRoomCta/index.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+"use client";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { type Dispatch, type SetStateAction } from "react";
+
+interface Props {
+  checkinDate: Date | null;
+  setCheckinDate: Dispatch<SetStateAction<Date | null>>;
+  checkoutDate: Date | null;
+  setCheckoutDate: Dispatch<SetStateAction<Date | null>>;
+  setAdults: Dispatch<SetStateAction<number>>;
+  setNoOfChildren: Dispatch<SetStateAction<number>>;
+  calcMinCheckoutDate: () => Date | null;
+  price: number;
+  discount: number;
+  adults: number;
+  noOfChildren: number;
+  specialNote: string;
+  isBooked: boolean;
+  handleBookNowClick: () => void;
+}
+
+export default function BookRoomCta({
+  price,
+  discount,
+  specialNote,
+  checkinDate,
+  setCheckinDate,
+  checkoutDate,
+  setCheckoutDate,
+  calcMinCheckoutDate,
+  setAdults,
+  setNoOfChildren,
+  adults,
+  noOfChildren,
+  isBooked,
+  handleBookNowClick,
+}: Props) {
+  const discountPrice = price - (price / 100) * discount;
+
+  const calcNoOfDays = () => {
+    if (!checkinDate || !checkoutDate) return 0;
+    const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
+
+    const noOfDays = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    return noOfDays;
+  };
+
+  return (
+    <div className="px-7 py-6 ">
+      <h3>
+        <span
+          className={`${
+            discount !== 0 ? "text-gray-400" : ""
+          } text-xl font-bold `}
+        >
+          R$ {price}/Dia
+        </span>
+        {discount !== 0 ? (
+          <span className="text-xl font-bold">
+            {" "}
+            | desconto {discount}% Apenas por :{" "}
+            <span className="text-tertiary-dark">R$ {discountPrice}/Dia</span>
+          </span>
+        ) : (
+          ""
+        )}
+      </h3>
+      <div className="my-2 w-full border-b-2 border-b-secondary" />
+      <h4 className="my-8 ">{specialNote}</h4>
+      <div className="flex ">
+        <div className="w-1/2 pr-2 ">
+          <label
+            htmlFor="check-in-date"
+            className="block text-sm font-medium text-gray-900 dark:text-gray-400"
+          >
+            Data de Entrada
+          </label>
+          <DatePicker
+            selected={checkinDate}
+            onChange={(date) => {
+              setCheckinDate(date);
+            }}
+            dateFormat="dd/MM/yyyy"
+            minDate={new Date()}
+            id="check-in-date"
+            className="w-full rounded-lg border border-gray-300 p-2.5 text-black focus:border-primary focus:ring-primary"
+          />
+        </div>
+        <div className="w-1/2 pr-2 ">
+          <label
+            htmlFor="check-out-date"
+            className="block text-sm font-medium text-gray-900 dark:text-gray-400"
+          >
+            Data de Saida
+          </label>
+          <DatePicker
+            selected={checkoutDate}
+            onChange={(date) => {
+              setCheckoutDate(date);
+            }}
+            dateFormat="dd/MM/yyyy"
+            minDate={calcMinCheckoutDate()}
+            disabled={!checkinDate}
+            id="check-out-date"
+            className="w-full rounded-lg border border-gray-300 p-2.5 text-black focus:border-primary focus:ring-primary"
+          />
+        </div>
+      </div>
+
+      <div className="mt-4 flex ">
+        <div className="w-1/2 pr-2 ">
+          <label
+            htmlFor="adults"
+            className="block text-sm font-medium text-gray-900 dark:text-gray-400"
+          >
+            Adultos
+          </label>
+          <input
+            type="number"
+            id="adults"
+            value={adults}
+            onChange={(e) => {
+              setAdults(Number(e.target.value));
+            }}
+            min={1}
+            max={5}
+            className="w-full rounded-lg border border-gray-300 p-2.5"
+          />
+        </div>
+        <div className="w-1/2 pl-2 ">
+          <label
+            htmlFor="children"
+            className="block text-sm font-medium text-gray-900 dark:text-gray-400"
+          >
+            Crianças
+          </label>
+          <input
+            type="number"
+            id="children"
+            value={noOfChildren}
+            onChange={(e) => {
+              setNoOfChildren(Number(e.target.value));
+            }}
+            min={1}
+            max={5}
+            className="w-full rounded-lg border border-gray-300 p-2.5"
+          />
+        </div>
+      </div>
+      {calcNoOfDays() > 0 ? (
+        <p className="mt-3"> Valor Total R$ {calcNoOfDays() * discountPrice}</p>
+      ) : (
+        <></>
+      )}
+      <button
+        disabled={isBooked}
+        onClick={handleBookNowClick}
+        className="btn-primary mt-6 w-full disabled:cursor-not-allowed disabled:bg-gray-500"
+      >
+        {isBooked ? "Não Disponível" : "Reserve Agora"}
+      </button>
+    </div>
+  );
+}
