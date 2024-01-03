af3cc509a8b921bfcc657eded9cebe9d6424f8c9
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-01-03 01:21:42 -0300

    🐛 Bug: forgoten href

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import CountUpNumber from "../CountUpNumber";
-
+import { useRouter } from "next/navigation";
 export default function HeroSection() {
+  const router = useRouter();
   return (
     <section className="container mx-auto flex items-center gap-12 px-4 ">
       <div className="h-full py-10 ">
@@ -11,7 +13,14 @@ export default function HeroSection() {
         <p className="mb-12 max-w-lg text-[#4a4a4a] dark:text-[#fff] ">
           Reserve seu hotel com a maior facilidade e segurança
         </p>
-        <button className="btn-primary">Reserve agora</button>
+        <button
+          className="btn-primary"
+          onClick={() => {
+            router.push(`/rooms`);
+          }}
+        >
+          Reserve agora
+        </button>
 
         <div className="mt-12 flex justify-between ">
           <div className="flex flex-col items-center justify-center gap-3">
